93d3c923ff901dc09130e98392f9cb488c3edbb1
Paul Kaefer <paul.kaefer@gmail.com>
2015-09-06 12:47:45 +0300

    started work on reading in files

--- glue.py
+++ glue.py
@@ -1,10 +1,35 @@
 # glue.py
 # 
 # "glue" a bunch of html files in a directory together
+#
+# read list of files in a directory: http://stackoverflow.com/questions/3207219/how-to-list-all-files-of-a-directory-in-python
 
 # get folder path from sys/argv
 
+from os import listdir
+from os.path import isfile, join
+from sys import argv
+
+folderPath = argv[1]
+
 # for each file in folder
+
+fileList = [ f for f in listdir(folderPath) if isfile(join(folderPath, f)) ]
+
+fileNumber = 1
+
+for fileName in fileList:
+    thisFile = open(fileName, 'r')
+    
+    if 1 == fileNumber:
+        keepReading = 1
+        for line in thisFile:
+            if -1 < string.find(line, "</body"):
+                keepReading = 0
+            
+    
+    thisFile.close()
+    fileNumber = fileNumber + 1
 
 # first one: read up until
 #   </body>
